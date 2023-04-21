9952a23a21a76568de487f5143084f9c8811ea1e
Svetlana Lazareva <slazareva.sl@gmail.com>
2023-04-21 13:34:08 +0300

    Initial commit

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -1,47 +1,46 @@
 package ru.netology;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class CardOrderTest {
+public class CardOrderTest {
 
-    private WebDriver driver;
+ private WebDriver driver;
 
-    @BeforeAll
-    static void setupAll() {
+ @BeforeAll
+    static void setUpAll() {
         WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999/");
     }
 
     @AfterEach
-    void teardown() {
+    void tearDown() {
         driver.quit();
         driver = null;
     }
 
     @Test
-     void testValid1() {
+    void test0() {
         driver.findElement(By.cssSelector("[data-test-id = name] input")).sendKeys("Светлана");
-        driver.findElement(By.cssSelector("[data-test-id = phone] input")).sendKeys("+79208651025");
+        driver.findElement(By.cssSelector("[data-test-id = phone] input")).sendKeys("+79208651085");
         driver.findElement(By.cssSelector("[data-test-id = agreement]")).click();
         driver.findElement(By.className("button")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
